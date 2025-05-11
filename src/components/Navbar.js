3ac1f39fe6a148d89807d16dd8b27f46079b6fde
Dogbone0714 <jameskang0714@gmail.com>
2025-05-11 16:55:41 +0800

    修改布局

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu } from 'antd';
 import { Link, useLocation } from 'react-router-dom';
+import { HomeOutlined, SearchOutlined, InfoCircleOutlined } from '@ant-design/icons';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -12,13 +13,13 @@ const Navbar = () => {
       selectedKeys={[location.pathname]}
       className="navbar"
     >
-      <Menu.Item key="/">
+      <Menu.Item key="/" icon={<HomeOutlined />}>
         <Link to="/">首頁</Link>
       </Menu.Item>
-      <Menu.Item key="/search">
+      <Menu.Item key="/search" icon={<SearchOutlined />}>
         <Link to="/search">企業查詢</Link>
       </Menu.Item>
-      <Menu.Item key="/about">
+      <Menu.Item key="/about" icon={<InfoCircleOutlined />}>
         <Link to="/about">關於我們</Link>
       </Menu.Item>
     </Menu>
